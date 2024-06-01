e77f84d0c9a924cb681fe60ea8863b7d0cdb61a9
astserval30 <astserval@gmail.com>
2024-06-02 00:53:42 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_for_1_task = number_of_hours/number_of_homework
+print ('Курс: ', course_name, ', всего задач: ', number_of_homework, ', затрачено часов: ', number_of_hours, ', среднее время выполнения ', time_for_1_task, 'часа.')
